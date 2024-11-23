c6eee77687785453ff8f99f49a21c098ddeb8615
arpan8 <arpanmajibally@gmail.com>
2024-11-23 22:53:05 +0530

    password generator initial

---
+++ react/react-coding-interview-questions/password-generator/README.md
@@ -0,0 +1,3 @@
+https://react-bootstrap.netlify.app/docs/components/table
+https://www.youtube.com/watch?v=u9-x0sG-WQc
+https://github.com/machadop1407/password-manager-react-mysql
---
+++ react/react-coding-interview-questions/password-generator/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button variant='success'>Hi</Button>
+    </>
+  )
+}
+
+export default App
