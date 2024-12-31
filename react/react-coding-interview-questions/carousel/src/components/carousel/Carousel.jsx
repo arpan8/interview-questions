9ac5e32cd37c391bd085acfae846f68b36ee0fbe
arpan8 <arpanmajibally@gmail.com>
2024-12-31 22:11:54 +0530

    slide show complete

--- react/react-coding-interview-questions/carousel/src/components/carousel/Carousel.jsx
+++ react/react-coding-interview-questions/carousel/src/components/carousel/Carousel.jsx
@@ -1,33 +1,80 @@
 import { useEffect, useState } from "react"
 import { getPhotosFromPicSum } from "./service"
-
+import { BallTriangle } from 'react-loader-spinner'
 const Carousel = () => {
     const [picsSumsList, setPicSumsList] = useState([])
     const [currentItemFromPicsSum, setCurrentItemFromPicsSum] = useState(0)
 
-    const fetchDataFromPicSum = async() => {
+    const fetchDataFromPicSum = async () => {
         const data = await getPhotosFromPicSum()
-        console.log(data, setCurrentItemFromPicsSum)
         setPicSumsList(data)
     }
-    useEffect(()=> {
+
+    const handlePrev = () => {
+        if (currentItemFromPicsSum === 0) {
+            setCurrentItemFromPicsSum(picsSumsList.length - 1)
+        } else {
+            setCurrentItemFromPicsSum((cur) => cur - 1)
+        }
+    }
+
+    const handleNext = () => {
+        if (currentItemFromPicsSum === picsSumsList.length - 1) {
+            setCurrentItemFromPicsSum(0)
+        } else {
+            setCurrentItemFromPicsSum((cur) => cur + 1)
+        }
+    }
+    useEffect(() => {
+        const timer = setInterval(() => {
+            handleNext()
+        }, 2000)
+        return () => clearInterval(timer)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentItemFromPicsSum])
+
+    useEffect(() => {
         fetchDataFromPicSum()
     }, [])
     return (
         <>
-        <pre>{JSON.stringify(picsSumsList)}</pre>
-        {picsSumsList.length > 0 ? <> 
-            <button>Prev</button>
-            <img 
-                height={picsSumsList[currentItemFromPicsSum].height}
-                width={picsSumsList[currentItemFromPicsSum].width}
-                src={picsSumsList[currentItemFromPicsSum].download_url}
-                alt={picsSumsList[currentItemFromPicsSum].author}
-            />
+            {/* {currentItemFromPicsSum} */}
+            {picsSumsList.length > 0 ? <>
+                <button onClick={handlePrev}>Prev</button>
+                <div>
+                    {picsSumsList[currentItemFromPicsSum]?.download_url ?
+                        <>
+                            <h2>
+                                <a href={picsSumsList[currentItemFromPicsSum]?.url} target="_blank">Check here</a>
+                            </h2>
 
-            <h2>{picsSumsList[currentItemFromPicsSum].author}</h2>
-            <button>Next</button>
-        </>: 'No data found'}
+                            <div>
+                                <img
+                                    // height={+picsSumsList[currentItemFromPicsSum].height / 10} // height is so big to reduce that divide by 10 and only whole number is taken
+                                    // width={+picsSumsList[currentItemFromPicsSum].width / 10} // weight is so big to reduce that divide by 10 and only whole number is taken
+                                    height={500}
+                                    width={500}
+                                    src={picsSumsList[currentItemFromPicsSum].download_url}
+                                    alt={picsSumsList[currentItemFromPicsSum].author}
+                                />
+                            </div>
+
+                        </>
+                        : 'No image found'}
+
+                </div>
+                <h2>{picsSumsList[currentItemFromPicsSum].author}</h2>
+                <button onClick={handleNext}>Next</button>
+            </> : <BallTriangle
+                height={100}
+                width={100}
+                radius={5}
+                color="#4fa94d"
+                ariaLabel="ball-triangle-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />}
         </>
     )
 }
